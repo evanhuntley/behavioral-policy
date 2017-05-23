78f31f21ae814aba668b0cdc93aae15ff1346b25
Evan Huntley <evan.huntley@gmail.com>
2017-05-23 08:35:48 -0400

    Reworking news page

--- billboard-in-action.php
+++
@@ -1,9 +0,0 @@
-<div class="billboard center in-action">
-    <div class="container">
-        <h2>Behavioral Science in Action</h2>
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/g_map.png" alt="BSPA in action" />
-        <div class="organizations">
-            <?= ot_get_option('in_action_billboard_text'); ?>
-        </div>
-    </div>
-</div>
--- page-news.php
+++ page-news.php
@@ -48,47 +48,62 @@ Template Name: News Page
             </div>
         </div>
 
-        <div class="news-section container">
+        <div class="news-main container">
             <div class="content">
-                <h2>In the News</h2>
-                <?php
-                $in_the_news = news_query('in-the-news');
+                <div class="news-section">
+                    <h2>In the News</h2>
+                    <?php
+                    $in_the_news = news_query('in-the-news');
 
-                if ($in_the_news->have_posts()) :
-                    ?>
-                    <ul>
-                        <?php while ( $in_the_news->have_posts() ) : $in_the_news->the_post(); ?>
-                            <?php get_template_part('loop', 'newsitem') ?>
-                        <?php endwhile;  ?>
-                    </ul>
-                <?php endif; wp_reset_query(); ?>
+                    if ($in_the_news->have_posts()) :
+                        ?>
+                        <ul>
+                            <?php while ( $in_the_news->have_posts() ) : $in_the_news->the_post(); ?>
+                                <?php get_template_part('loop', 'newsitem') ?>
+                            <?php endwhile;  ?>
+                        </ul>
+                    <?php endif; wp_reset_query(); ?>
                     <a class="more-link" href="/news-category/in-the-news">Show All</a>
-                <h2>BSPA in the News</h2>
-                <?php
-                $bsp_itn = news_query('bspa-in-the-news');
-
-                if ($bsp_itn->have_posts()) :
-                    ?>
-                    <ul>
-                        <?php while ( $bsp_itn->have_posts() ) : $bsp_itn->the_post(); ?>
-                            <?php get_template_part('loop', 'newsitem') ?>
-                        <?php endwhile;  ?>
-                    </ul>
-                <?php endif; wp_reset_query(); ?>
-                <a class="more-link" href="/news-category/bspa-in-the-news">Show All</a>
-                
-                <h2>Behavioral Science in Action</h2>
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/g_map.png" alt="BSPA in action" />
-                <div class="organizations">
-                    <?= ot_get_option('in_action_billboard_text'); ?>
                 </div>
+                <div class="news-section">
+                    <h2>BSPA in the News</h2>
+                    <?php
+                    $bsp_itn = news_query('bspa-in-the-news');
+
+                    if ($bsp_itn->have_posts()) :
+                        ?>
+                        <ul>
+                            <?php while ( $bsp_itn->have_posts() ) : $bsp_itn->the_post(); ?>
+                                <?php get_template_part('loop', 'newsitem') ?>
+                            <?php endwhile;  ?>
+                        </ul>
+                    <?php endif; wp_reset_query(); ?>
+                    <a class="more-link" href="/news-category/bspa-in-the-news">Show All</a>
+                </div>    
+            </div>
+        </div>
+        
+        <div class="billboard center light resources" id="resources" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')">
+            <div class="container">
+                <h2>resources</h2>
             </div>
         </div>
 
-        <div class="reports container" id="reports">
+        <div class="reports container">
             <div class="content">
+                <div class="in-action" id="in-action">
+                    <h2>Behavioral Science in Action</h2>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/g_map.png" alt="BSPA in action" />
+                    <div class="orgs">
+                        <?= types_render_field('behavioral-science-in-action'); ?>
+                    </diV>
+                </div>
+                <div class="online-database" id="online-database">
+                    <h2>Online Database</h2>
+                    <?= types_render_field('online-database'); ?>
+                </div>
                 <h2>Reports Corner</h2>
-                <div class="reports-details">
+                <div class="reports-details" id="reports">
                     <?= types_render_field('reports-corner-text'); ?>
                 </div>
                 <?php
@@ -111,51 +126,55 @@ Template Name: News Page
             </div>
         </div>
 
-        <div class="news-section additional-resources">
+        <div class="additional-resources" id="additional-resources">
             <div class="container">
                 <div class="content">
                     <h1>additional resources</h1>
-                    <h2>Recommended Reading</h2>
-                    <?php
-                    $reading = news_query('recommended-reading');
+                    <div class="news-section">
+                        <h2>Recommended Reading</h2>
+                        <?php
+                        $reading = news_query('recommended-reading');
 
-                    if ($reading->have_posts()) :
-                        ?>
-                        <ul>
-                            <?php while ( $reading->have_posts() ) : $reading->the_post(); ?>
-                                <?php get_template_part('loop', 'newsitem') ?>
-                            <?php endwhile;  ?>
-                        </ul>
-                    <?php endif; wp_reset_query(); ?>
-                    <a class="more-link" href="/news-category/recommended-reading">Show All</a>
+                        if ($reading->have_posts()) :
+                            ?>
+                            <ul>
+                                <?php while ( $reading->have_posts() ) : $reading->the_post(); ?>
+                                    <?php get_template_part('loop', 'newsitem') ?>
+                                <?php endwhile;  ?>
+                            </ul>
+                        <?php endif; wp_reset_query(); ?>
+                        <a class="more-link" href="/news-category/recommended-reading">Show All</a>
+                    </div>
+                    <div class="news-section">
+                        <h2>Free Resources</h2>
+                        <?php
+                        $free = news_query('free-resources');
 
-                    <h2>Free Resources</h2>
-                    <?php
-                    $free = news_query('free-resources');
+                        if ($free->have_posts()) :
+                            ?>
+                            <ul>
+                                <?php while ( $free->have_posts() ) : $free->the_post(); ?>
+                                    <?php get_template_part('loop', 'newsitem') ?>
+                                <?php endwhile;  ?>
+                            </ul>
+                        <?php endif; wp_reset_query(); ?>
+                        <a class="more-link" href="/news-category/free-resources">Show All</a>
+                    </div>
+                    <div class="news-section">
+                        <h2>Exec Ed/Continuing Professional Development</h2>
+                        <?php
+                        $exec = news_query('exec-ed');
 
-                    if ($free->have_posts()) :
-                        ?>
-                        <ul>
-                            <?php while ( $free->have_posts() ) : $free->the_post(); ?>
-                                <?php get_template_part('loop', 'newsitem') ?>
-                            <?php endwhile;  ?>
-                        </ul>
-                    <?php endif; wp_reset_query(); ?>
-                    <a class="more-link" href="/news-category/free-resources">Show All</a>
-                    
-                    <h2>Exec Ed/Continuing Professional Development</h2>
-                    <?php
-                    $exec = news_query('exec-ed');
-
-                    if ($exec->have_posts()) :
-                        ?>
-                        <ul>
-                            <?php while ( $exec->have_posts() ) : $exec->the_post(); ?>
-                                <?php get_template_part('loop', 'newsitem') ?>
-                            <?php endwhile;  ?>
-                        </ul>
-                    <?php endif; wp_reset_query(); ?>
-                    <a class="more-link" href="/news-category/exec-ed">Show All</a>
+                        if ($exec->have_posts()) :
+                            ?>
+                            <ul>
+                                <?php while ( $exec->have_posts() ) : $exec->the_post(); ?>
+                                    <?php get_template_part('loop', 'newsitem') ?>
+                                <?php endwhile;  ?>
+                            </ul>
+                        <?php endif; wp_reset_query(); ?>
+                        <a class="more-link" href="/news-category/exec-ed">Show All</a>
+                    </div>
                 </div>
             </div>
         </div>

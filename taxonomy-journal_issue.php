bba0f1bb93aa81e4767d8876c7fc618dcab35376
Evan Huntley <evan.huntley@gmail.com>
2020-06-06 13:04:23 -0400

    Adding loop for journal

--- taxonomy-journal_issue.php
+++ taxonomy-journal_issue.php
@@ -10,15 +10,15 @@
         
         <div class="page-header" style="background-image: url('/wp-content/uploads/2017/01/bg_home-bsp.jpg');">
             <div class="container">
-                <h1><?php single_term_title(); ?></h1>
+                <h1>BSP: <?php single_term_title(); ?></h1>
                 <p><?php echo term_description(); ?></p>
             </div>
         </div>
 
         <div class="primary container">
             <section class="content">
-                <div class="editors">
-                    <h2>Journal Issues</h2>                 
+                <div class="featured-articles">
+                    <?php get_template_part( 'loop', 'journal' ); ?>
                 </div>
             </section>
             <?php get_sidebar(); ?>
